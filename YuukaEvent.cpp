b00da952748091bc7f80951ae2d683e5c3c2fda6
Vekolyram <vekooolyyyram@outlook.com>
2025-03-15 15:09:10 +0800

    a

--- YuukaEvent.cpp
+++ YuukaEvent.cpp
@@ -3,43 +3,54 @@
 
 #include <iostream>
 #include <map>
-#include <vector>
 #include <functional>
 #include <future>
 #include <queue>
 #include <atomic>
-typedef int Event;
 enum class EventID { Null, OnGroupMsg, OnFriendMsg };
-class EventBus {
-	std::map<EventID, std::queue<std::function<void(const Event&& event)>>> events;
-	std::atomic<bool> lock;
+enum class EventQueue { AtOnce, Delay };
+enum class EventPriority { Low, Normal, High };
+enum class EventMode { Count, Repeat };
+class Event {
 public:
-	EventBus() : events() {}
-	void addEvent(EventID id, std::function<void(const Event&& event)> func) {
+	const EventID id;
+	std::function<void(const Event& event)> func;
+	Event(EventID id) : id(id) {}
+	void operator()(const Event& event) {
+		func(*this);
+	}
+};
+class EventBus {
+	std::map<EventID, std::queue<Event>> events;
+	std::map<EventID, bool> locks;
+	std::atomic<bool> bigLock;
+public:
+	EventBus() : bigLock(false) {}
+	void addEvent(EventID id, std::function<void(const Event& event)> func) {
 		events[id].emplace(func);
 	}
-	void removeEvent(EventID id, std::function<void(const Event&& event)> func) {
-		std::queue<std::function<void(const Event&& event)>> nil;
+	void removeEvent(EventID id, std::function<void(const Event& event)> func) {
+		std::queue<Event> nil;
 		events[id].swap(nil);
 	}
 	void enqueueEvent(EventID id) {
 		std::async(std::launch::async, [=]() {
+			while (locks[id]);
+			locks[id] = true;
 			for (;events[id].empty();events[id].pop()) {
-				events[id].front();
+				events[id].front()(Event(id));
 			}
+			locks[id] = false;
 			std::cout << "event done" << std::endl;
 			});
 	}
 };
-//auto a() {
-//	return Event([](const Event&& event) {
-//		std::cout << static_cast<int>(event.id) << std::endl;
-//		}, EventID::OnGroupMsg);
-//}
 int main()
 {
 	//Event e1 = a();
 	//e1();
-	Event
-		std::cout << "Hello World!\n";
+	EventBus bus;
+	bus.addEvent(EventID::OnGroupMsg, [](const Event& event) {});
+	bus.enqueueEvent(EventID::OnGroupMsg);
+	std::cout << "Hello World!\n";
 }
