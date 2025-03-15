a79f36384be2d9bc1dee9821f5155fbe4eb65f84
Vekolyram <vekooolyyyram@outlook.com>
2025-03-15 10:00:41 +0800

    a

--- YuukaEvent.cpp
+++ YuukaEvent.cpp
@@ -8,42 +8,38 @@
 #include <future>
 #include <queue>
 #include <atomic>
+typedef int Event;
 enum class EventID { Null, OnGroupMsg, OnFriendMsg };
-class Event
-{
-public:
-	const EventID id = EventID::Null;
-	std::queue<std::function<void(const Event& event)>> funcs;
-	Event(std::function<void(const Event& event)> func, EventID id) : id(id) {
-		funcs.emplace(func);
-	}
-	void operator()() {
-	}
-	~Event() {
-	}
-};
 class EventBus {
-	std::map<EventID, std::queue<std::function<void(const Event& event)>>> events;
+	std::map<EventID, std::queue<std::function<void(const Event&& event)>>> events;
 	std::atomic<bool> lock;
 public:
 	EventBus() : events() {}
-	void addEvent(EventID id, std::function<void(const Event& event)> func) {
+	void addEvent(EventID id, std::function<void(const Event&& event)> func) {
 		events[id].emplace(func);
 	}
-	void removeEvent(EventID id, std::function<void(const Event& event)> func) {
-		std::queue<std::function<void(const Event& event)>> nil;
+	void removeEvent(EventID id, std::function<void(const Event&& event)> func) {
+		std::queue<std::function<void(const Event&& event)>> nil;
 		events[id].swap(nil);
 	}
-	void
+	void enqueueEvent(EventID id) {
+		std::async(std::launch::async, [=]() {
+			for (;events[id].empty();events[id].pop()) {
+				events[id].front();
+			}
+			std::cout << "event done" << std::endl;
+			});
+	}
 };
-auto a() {
-	return Event([](const Event& event) {
-		std::cout << static_cast<int>(event.id) << std::endl;
-		}, EventID::OnGroupMsg);
-}
+//auto a() {
+//	return Event([](const Event&& event) {
+//		std::cout << static_cast<int>(event.id) << std::endl;
+//		}, EventID::OnGroupMsg);
+//}
 int main()
 {
-	Event e1 = a();
-	e1();
-	std::cout << "Hello World!\n";
+	//Event e1 = a();
+	//e1();
+	Event
+		std::cout << "Hello World!\n";
 }
